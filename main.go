6b3d53efdd93b8b4155c26a2d54ab51ddbc4fe3e
Bharath Vedartham <vedabharath12345@gmail.com>
2023-11-07 21:41:26 +0530

    broadcast-2 and broadcast-3

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -18,6 +20,23 @@ type NetworkTopologyBody struct {
 
 type NetworkTopologyResponse struct {
 	Type string `json:"type"`
+}
+
+type BroadcastRequest struct {
+	Source string        `json:"src"`
+	Dest   string        `json:"dst"`
+	Body   BroadcastBody `json:"body"`
+}
+
+type BulkBroadcastRequest struct {
+	Source string            `json:"src"`
+	Dest   string            `json:"dst"`
+	Body   BulkBroadcastBody `json:"body"`
+}
+
+type BulkBroadcastBody struct {
+	Type     string `json:"type"`
+	Messages []int  `json:"messages"`
 }
 
 type BroadcastBody struct {
@@ -40,8 +59,24 @@ type ReadResponse struct {
 
 func main() {
 	n := maelstrom.NewNode()
+	messageMutex := sync.RWMutex{}
 	messages := map[int]bool{}
 	networkTopology := NetworkTopology{Nodes: map[string][]string{}}
+
+	n.Handle("bulk_broadcast", func(msg maelstrom.Message) error {
+		var body BulkBroadcastBody
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		messageMutex.Lock()
+		for _, v := range body.Messages {
+			messages[v] = true
+		}
+		messageMutex.Unlock()
+
+		return nil
+	})
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -50,7 +85,9 @@ func main() {
 			return err
 		}
 
+		messageMutex.Lock()
 		messages[body.Message] = true
+		messageMutex.Unlock()
 
 		// Update the message type to return back.
 		response := BroadcastResponse{Type: "broadcast_ok"}
@@ -67,9 +104,11 @@ func main() {
 		}
 
 		messageRes := []int{}
+		messageMutex.RLock()
 		for k, _ := range messages {
 			messageRes = append(messageRes, k)
 		}
+		messageMutex.RUnlock()
 
 		// Update the message type to return back.
 		response := ReadResponse{
@@ -96,6 +135,29 @@ func main() {
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, response)
 	})
+
+	go func() {
+		for {
+			nodeIds := n.NodeIDs()
+			messagesToBroadcast := []int{}
+			messageMutex.RLock()
+			for k, _ := range messages {
+				messagesToBroadcast = append(messagesToBroadcast, k)
+			}
+			messageMutex.RUnlock()
+			for _, id := range nodeIds {
+				err := n.Send(id, BulkBroadcastBody{
+					Type:     "bulk_broadcast",
+					Messages: messagesToBroadcast,
+				})
+				if err != nil {
+					log.Printf("reply error: %s", err)
+				}
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+	}()
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
